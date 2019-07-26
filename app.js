cb1bac21e182e77584b92477c07cda17a3e75d4d
mdaf97 <52542467+mdaf97@users.noreply.github.com>
2019-07-26 19:31:08 +0100

    Added a "return to top" button

--- app.js
+++ app.js
@@ -74,3 +74,17 @@ window.onload=function getTrendingData() {
       
     });
 }
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 10) {       // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
